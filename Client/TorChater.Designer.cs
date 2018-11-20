bb1507c5920ab7adbd39ac08a1eff07e9b9b52e6
Rabea Zidan <rabea0102.ghost@gmail.com>
2018-11-20 22:35:03 +0200

    client 1.0

--- Client/TorChater.Designer.cs
+++ Client/TorChater.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.exitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(50, 274);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(120, 45);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // TorChater
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(496, 359);
+            this.Controls.Add(this.exitButton);
             this.Name = "TorChater";
             this.Text = "TorChater";
+            this.Load += new System.EventHandler(this.TorChater_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
