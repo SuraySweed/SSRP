4846764459e45aee8c8abc3df51fa75b7265a071
Rabea Zidan <rabea0102.ghost@gmail.com>
2018-12-19 23:05:14 +0200

    better client and a little better server

--- Server/TorChatServer.py
+++ Server/TorChatServer.py
@@ -6,13 +6,14 @@ PORT = 8820
 users_dictionary = {}
 
 def main():
-	
+
 	client_name = ""
 	
 	listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind the socket to the port
 	server_address = ('localhost', 8820)
+	print("WELCOME TO THE SSRP SERVER !!\n-----------------------\n")
 	print ("starting up on port", server_address)
 	listening_socket.bind(server_address)
 
@@ -27,26 +28,28 @@ def main():
 			connection, client_address = listening_socket.accept()
 			print('connection from', client_address)
 
-			msg_from_server = connection.recv(128)
-			print("received: ", msg_from_server.decode('utf-8'))
-			msg_code = get_msg_code(msg_from_server)
+			msg_from_client = connection.recv(128)
+			print("received: ", msg_from_client.decode('utf-8'))
+			msg_code = get_msg_code(msg_from_client.decode('utf-8'))
 		
 			if(msg_code == 100):
-				client_name = handle_recevied_name(msg_from_server)	
+				client_name = handle_recevied_name(msg_from_client.decode('utf-8'))	
 			
-			msg_to_client = b'("HEY ", client_name, ", WELCOME TO THE SSRP SERVER !!!")'
-			connection.sendall(msg_to_client)
+			#msg_to_client = "HEY ", client_name, ", WELCOME TO THE SSRP SERVER !!!"
+			connection.sendall(b"WELCOME TO THE SSRP SERVER !!!")
 			
-			msg_from_server = connection.recv(256)
-			msg_code = get_msg_code(msg_from_server)
-			if(msg_code == 102):
-				msg_to_client = handle_forwarding_information_request(msg_from_server)
-				connection.sendall(msg_to_client)
+			msg_from_client = connection.recv(256)
+			print("received: ", msg_from_client.decode('utf-8'))
+			msg_code = get_msg_code(msg_from_client)
+			if(msg_code == b'102'):
+				msg_to_client = handle_forwarding_information_request(msg_from_client)
+				print("sent: ", msg_to_client)
+				connection.sendall(msg_to_client.encode())
 			
 			
 			users_dictionary.update({client_name : client_address})		
 			for key,value in users_dictionary.items():
-				print(key + ", " + value)
+				print(key + ", " + value[0] + " " + str(value[1]))
 		
 
 
@@ -65,8 +68,8 @@ def main():
 			listening_socket.close()
      	 
 
-def get_msg_code(msg_from_server):
-	return msg_from_server[0:3]
+def get_msg_code(msg_from_client):
+	return msg_from_client[0:3]
 		 
 	 
 def handle_recevied_name(msg):
@@ -78,7 +81,7 @@ def handle_forwarding_information_request(msg):
 	other_side_client_name = (msg.split("|"))[1]
 	for key in users_dictionary.keys():
 		if(other_side_client_name == key):
-			msg_to_client = "201|" + users_dictionary[key][0] + "|" + users_dictionary[key][1]
+			msg_to_client = "201|" + users_dictionary[key][0] + "|" + str(users_dictionary[key][1])
 		
 		else:
 			msg_to_client = "201|0" # false
