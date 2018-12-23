498aa5d7ecdd86045ae2ae9d53524b5e0d8b3a2f
SuraySweed <suray263@gmail.com>
2018-12-23 19:24:35 +0200

    server 2.1

--- Server/TorChatServer.py
+++ Server/TorChatServer.py
@@ -4,10 +4,9 @@ HOST = '127.0.0.1'
 PORT = 8820
 
 users_dictionary = {}
+list
 
 def main():
-
-	client_name = ""
 	
 	listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -27,46 +26,19 @@ def main():
 			
 			connection, client_address = listening_socket.accept()
 			print('connection from', client_address)
-
-			msg_from_client = connection.recv(128)
-			print("received: ", msg_from_client.decode('utf-8'))
-			msg_code = get_msg_code(msg_from_client)
-		
-			if(msg_code == b'100'):
-				client_name = handle_recevied_name(msg_from_client)	
 			
-			msg_to_client = "HEY " + client_name + ", WELCOME TO THE SSRP SERVER !!!"
-			print("send: ", msg_to_client)
-			print("-----------------------")
-			connection.sendall(msg_to_client.encode())
-			
-			print("CONNECTED USERS:")
-			users_dictionary.update({client_name : client_address})		
-			for key,value in users_dictionary.items():
-				print(key + ", " + value[0] + " " + str(value[1]))
-			print("-----------------------")
-			
-			msg_from_client = connection.recv(256)
-			print("received: ", msg_from_client.decode('utf-8'))
-			msg_code = get_msg_code(msg_from_client)
-			if(msg_code == b'102'):
-				msg_to_client = handle_forwarding_information_request(msg_from_client)
-				print("sent: ", msg_to_client)
-				print("-----------------------")
-				connection.sendall(msg_to_client.encode())
-			
-
-
-			data = connection.recv(2048)
-			print("received: ", data.decode('utf-8'))
-			if data:
-				print('sending data back to the client')
-				print("-----------------------")
-				connection.sendall(data)
-			else:
-				print('no more data from', client_address)
-				break
-
+			while(True):
+				msg_from_client = connection.recv(2048)
+				
+				if msg_from_client:
+					print("Received: ", msg_from_client.decode('utf-8'))
+					msg_code = get_msg_code(msg_from_client)
+					handle_msg_from_client(msg_code, msg_from_client, connection, client_address)
+				
+				else:
+					print("no more data from", client_address)
+					break
+				
 		finally:
             # Clean up the connection
 			connection.close()
@@ -77,21 +49,41 @@ def get_msg_code(msg_from_client):
 	return msg_from_client[0:3]
 		 
 	 
-def handle_recevied_name(msg):
-	return ((msg.decode('utf-8').split("|"))[1])
-
+def handle_recevied_name(msg, connection, client_address):
+	client_name = (msg.decode('utf-8').split("|"))[1]
+	users_dictionary.update({client_name : client_address})		
 	
-def handle_forwarding_information_request(msg):
-	msg_to_client = "201|0" #false
+	msg_to_client = "201|HEY " + client_name + ", WELCOME TO THE SSRP SERVER !!!"
+	print("send: ", msg_to_client)
+	print("-----------------------")
+	connection.sendall(msg_to_client.encode())
 	
-	other_side_client_name = ((msg.decode('utf-8')).split("|"))[1]
+	None 
+	
+def handle_forwarding_information_request(msg_from_client, connection, client_address):
+	
+	msg_to_client = "203|0" #false
+	
+	other_side_client_name = ((msg_from_client.decode('utf-8')).split("|"))[1]
 	for key in users_dictionary.keys():
 		if(other_side_client_name == key):
-			msg_to_client = "201|" + users_dictionary[key][0] + "|" + str(users_dictionary[key][1])
+			msg_to_client = "203|" + users_dictionary[key][0] + "|" + str(users_dictionary[key][1])
 			
-		
-	return msg_to_client
+	print("sent: ", msg_to_client)
+	print("-----------------------")
+
+	connection.sendall(msg_to_client.encode())
 	
+	None
+
+
+def handle_msg_from_client(msg_code, msg_from_client, connection, client_address):
+	
+	if(msg_code == b'100'):
+		client_name = handle_recevied_name(msg_from_client, connection, client_address)
+	
+	elif(msg_code == b'102'):
+		handle_forwarding_information_request(msg_from_client, connection, client_address)
 		
-		
+	
 main()
